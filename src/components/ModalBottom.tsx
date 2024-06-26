1240ce6a43bdf89366872eddf0494880bf5f2bcd
jennyjoo <100cllr@gmail.com>
2024-06-26 13:36:31 +0900

    feat: modalBottom 구현

---
+++ src/components/ModalBottom.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import BtnBottom from './BtnBottom';
+
+type Prop = {
+  title?: string;
+  btnBottom?: boolean;
+  action?: { (): void }[];
+  btnText?: string;
+  children: ReactNode;
+};
+
+const ModalBottom = ({ title, btnBottom, action, btnText, children }: Prop) => {
+  return (
+    <div className='fixed bottom-0 w-[390px]  bg-white rounded-t-2xl drop-shadow-sm min-h-[200px] flex flex-col justify-between px-3 py-5 '>
+      {title && <div className='font-black mb-2'>{title}</div>}
+      <div>{children}</div>
+      {btnBottom && (
+        <div>
+          <BtnBottom text={btnText || ''} action={action ?? []} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ModalBottom;
