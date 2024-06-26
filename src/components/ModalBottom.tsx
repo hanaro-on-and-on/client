b95b572a83d453b48cfdc4a1dbace26b80e6a8b5
jennyjoo <100cllr@gmail.com>
2024-06-26 15:00:48 +0900

    refactor: refactor "isOpen"

--- src/components/ModalBottom.tsx
+++ src/components/ModalBottom.tsx
@@ -7,19 +7,34 @@ type Prop = {
   action?: { (): void }[];
   btnText?: string;
   children: ReactNode;
+  closeModal: () => void;
 };
 
-const ModalBottom = ({ title, btnBottom, action, btnText, children }: Prop) => {
+const ModalBottom = ({
+  title,
+  btnBottom,
+  action,
+  btnText,
+  closeModal,
+  children,
+}: Prop) => {
   return (
-    <div className='fixed bottom-0 w-[390px]  bg-white rounded-t-3xl drop-shadow-sm min-h-[200px] flex flex-col justify-between px-3 pt-10 pb-5 '>
-      {title && <div className='font-black mb-2'>{title}</div>}
-      <div>{children}</div>
-      {btnBottom && (
-        <div>
-          <BtnBottom text={btnText || ''} action={action ?? []} />
+    <>
+      <div className='fixed w-[390px] h-screen bg-black bg-opacity-50 z-40 flex justify-center items-center'>
+        <div className='fixed bottom-0 w-[390px]  bg-white rounded-t-3xl drop-shadow-sm min-h-[200px] flex flex-col justify-between px-5 pt-10 pb-5 '>
+          {title && <div className='font-black mb-2'>{title}</div>}
+          <div>{children}</div>
+          {btnBottom && (
+            <div>
+              <BtnBottom
+                text={btnText || ''}
+                action={[...(action ?? []), closeModal]}
+              />
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,26 +1,36 @@
+import { useState } from 'react';
 import BtnBottom from '../components/BtnBottom';
 import Frame from '../components/Frame';
 import ModalBottom from '../components/ModalBottom';
 
 const Test = () => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(true);
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   const func = () => {
     console.log('안녕하세요');
   };
   return (
     <>
       <Frame navTitle='알바ON'>
-        <ModalBottom
-          title='뒤지겠니?'
-          btnBottom
-          btnText='아이스티'
-          action={[
-            () => {
-              console.log('아이스티 미지근');
-            },
-          ]}
-        >
-          <div>뒤지겠다</div>
-        </ModalBottom>
+        {isModalOpen && (
+          <ModalBottom
+            title='뒤지겠니?'
+            btnBottom
+            btnText='아이스티'
+            closeModal={closeModal}
+            action={[
+              () => {
+                console.log('아이스티 미지근');
+              },
+            ]}
+          >
+            <div>뒤지겠다</div>
+          </ModalBottom>
+        )}
         <div className='text-2xl font-bold text-hanaLightGreen'>test</div>
         <BtnBottom text='버튼' action={[func]}></BtnBottom>
       </Frame>
