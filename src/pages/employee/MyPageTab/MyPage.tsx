e8e45b0ced49c5d65028a5552f0617e07447ef57
jennyjoo <100cllr@gmail.com>
2024-07-01 17:26:34 +0900

    feat: 전자서명 구현

--- src/pages/employee/MyPageTab/MyPage.tsx
+++ src/pages/employee/MyPageTab/MyPage.tsx
@@ -71,7 +71,10 @@ const MyPage = () => {
               >
                 <div className='flex flex-col justify-center text-sm w-full'>
                   <div className='w-full flex justify-between items-center'>
-                    <WorkPlaceName name={item.workPlaceNm} colorType='02' />
+                    <WorkPlaceName
+                      name={item.workPlaceNm}
+                      colorType={item.colorTypeCd}
+                    />
                     <div>{item.employmentContractCreatedAt}</div>
                   </div>
                 </div>
---
+++ src/types/sign.d.ts
@@ -0,0 +1,3 @@
+type SignPadHandler = {
+  canvasRef: RefObject<any>;
+};
