e42540a0ab6ea77662320f9f3d90230db4f36ccc
jennyjoo <100cllr@gmail.com>
2024-07-01 16:00:52 +0900

    style: 모달 뒷부분 터치시 꺼지게 만들기
    modalBottom

--- src/components/ModalBottom.tsx
+++ src/components/ModalBottom.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { ReactNode } from 'react';
 import BtnBottom from './BtnBottom';
 
@@ -20,7 +22,10 @@ const ModalBottom = ({
 }: Prop) => {
   return (
     <>
-      <div className='fixed bottom-0 self-center w-[390px] h-screen bg-black bg-opacity-50 z-40 flex justify-center items-center'>
+      <div
+        className='fixed bottom-0 self-center w-[390px] h-screen bg-black bg-opacity-50 z-40 flex justify-center items-center'
+        onClick={closeModal}
+      >
         <div className='fixed bottom-0 w-[390px]  bg-white rounded-t-3xl drop-shadow-sm min-h-[200px] flex flex-col justify-between px-5 pt-0 pb-5 '>
           <button
             className='w-full py-3 flex justify-center bg-transparent'
