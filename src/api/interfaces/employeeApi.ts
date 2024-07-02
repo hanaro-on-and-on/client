82925efaa64a59ce60fa0ba39ccd958638757057
jennyjoo <100cllr@gmail.com>
2024-07-02 13:03:39 +0900

    feat: 알바생 급여명세서 수령확인 서명

--- src/api/interfaces/employeeApi.ts
+++ src/api/interfaces/employeeApi.ts
@@ -19,6 +19,8 @@ interface employeeApi {
     yearn: number,
     month: number
   ): Promise<EmployeePayStubGetResponse>;
+
+  employeeSignature(payStubId: number): Promise<EmployeeSignatureResponse>;
 }
 
 export default employeeApi;
