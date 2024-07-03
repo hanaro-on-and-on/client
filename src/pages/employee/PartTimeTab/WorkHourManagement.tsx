aa74aca63155efdc829bbe4dbee4494ef6bf8391
jennyjoo <100cllr@gmail.com>
2024-07-03 16:40:20 +0900

    fix: 근무 내역 컴포넌틔 매개변수 추가

--- src/pages/employee/PartTimeTab/WorkHourManagement.tsx
+++ src/pages/employee/PartTimeTab/WorkHourManagement.tsx
@@ -1,19 +1,41 @@
 import { useNavigate } from 'react-router-dom';
 import WhiteBox from '../../../components/ui/WhiteBox';
+import { ChangeEvent } from 'react';
+import { useDate } from '../../../contexts/Date-Context';
+import { formatMonths, styleMonths } from '../../../utils/format-date';
 
 type Prop = {
   year: number;
   month: number;
+  id: number;
+  monthList: Date[];
 };
 
-const WorkHourManagement = ({ year, month }: Prop) => {
+const WorkHourManagement = ({ year, month, id, monthList }: Prop) => {
   const navigate = useNavigate();
+  const { date, setYearMonth } = useDate();
 
   return (
     <>
       {month && (
-        <WhiteBox className='w-full py-5' border>
-          <div></div>
+        <WhiteBox className='py-3 px-3 w-full border'>
+          <select
+            defaultValue={date.toString()}
+            onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+              const sel = new Date(e.target.value);
+              setYearMonth(sel);
+            }}
+          >
+            {monthList.map((dat) => (
+              <option
+                selected={date === dat}
+                key={dat.toISOString()}
+                value={dat.toString()}
+              >
+                {`${styleMonths(formatMonths(dat))} 근무 내역`}
+              </option>
+            ))}
+          </select>
         </WhiteBox>
       )}
     </>
