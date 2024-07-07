79aa8f262e36c945f0e34de975e6b3114ea2f913
jennyjoo <100cllr@gmail.com>
2024-07-07 17:31:02 +0900

    feat: 월별 근무 목록 화면 구혀

--- src/pages/employee/PartTimeTab/WorkHourManagement.tsx
+++ src/pages/employee/PartTimeTab/WorkHourManagement.tsx
@@ -1,9 +1,10 @@
-import { useNavigate } from 'react-router-dom';
 import WhiteBox from '../../../components/ui/WhiteBox';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { useDate } from '../../../contexts/Date-Context';
 import { formatMonths, styleMonths } from '../../../utils/format-date';
 import ApiClient from '../../../api/apiClient';
+import WorkTimeList from '../../../components/employee/WorkTimeList';
+import getTimeGap from '../../../utils/get-time-gap';
 
 type Prop = {
   year: number;
@@ -13,11 +14,10 @@ type Prop = {
 };
 
 const WorkHourManagement = ({ year, month, id, monthList }: Prop) => {
-  const navigate = useNavigate();
   const { date, setYearMonth } = useDate();
 
-  const [connectedList, setConnectedList] = useState<WorkListWorkTime[]>([]);
-  const [worktimeList, setWorkTimeList] = useState<WorkListWorkTime[]>([]);
+  const [connectedList, setConnectedList] =
+    useState<EmployeeWorkTimeListConnected | null>(null);
 
   const fetchConnData = async () => {
     try {
@@ -29,54 +29,71 @@ const WorkHourManagement = ({ year, month, id, monthList }: Prop) => {
         );
       console.log('conn res', response);
 
-      setConnectedList(response.works);
+      setConnectedList(response);
     } catch (err) {
       console.log(err);
     }
   };
 
-  const fetchData = async () => {
-    try {
-      const response = await ApiClient.getInstance().employeeGetWorkTimeList(
-        id,
-        year,
-        month
-      );
-      console.log('res', response);
+  const getTotalWorktime = (): number => {
+    let total = 0;
+    connectedList?.works?.forEach(
+      (item) => (total += getTimeGap(item.startTime, item.endTime))
+    );
 
-      setWorkTimeList(response.works);
-    } catch (err) {
-      console.log(err);
-    }
+    return total;
+  };
+
+  const formatTime = (totalMinutes: number): string => {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = Math.floor(totalMinutes % 60);
+    return minutes === 0 ? `${hours}시간` : `${hours}시간 ${minutes}분`;
   };
 
   useEffect(() => {
     fetchConnData();
-    fetchData();
   }, []);
 
   return (
     <>
       {month && (
-        <WhiteBox className='py-3 px-3 w-full border'>
-          <select
-            defaultValue={date.toString()}
-            onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-              const sel = new Date(e.target.value);
-              setYearMonth(sel);
-            }}
-          >
-            {monthList.map((dat) => (
-              <option
-                selected={date === dat}
-                key={dat.toISOString()}
-                value={dat.toString()}
+        <>
+          <WhiteBox className='py-3 px-3 w-full border'>
+            <div>
+              <select
+                defaultValue={date.toString()}
+                onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+                  const sel = new Date(e.target.value);
+                  setYearMonth(sel);
+                }}
               >
-                {`${styleMonths(formatMonths(dat))} 근무 내역`}
-              </option>
-            ))}
-          </select>
-        </WhiteBox>
+                {monthList.map((dat) => (
+                  <option
+                    selected={date === dat}
+                    key={dat.toISOString()}
+                    value={dat.toString()}
+                  >
+                    {`${styleMonths(formatMonths(dat))} 근무 내역`}
+                  </option>
+                ))}
+              </select>
+              {connectedList && (
+                <div className='text-2xl font-bold pt-3'>
+                  {formatTime(getTotalWorktime())}
+                </div>
+              )}
+            </div>
+          </WhiteBox>
+          {connectedList && (
+            <WorkTimeList
+              key={connectedList.PlaceId}
+              PlaceId={connectedList.PlaceId}
+              workPlaceNm={connectedList.workPlaceNm}
+              colorTypeCd={connectedList.workPlaceColor}
+              works={connectedList.works}
+            />
+          )}
+        </>
       )}
     </>
   );
