583b355440948bd7a692d733e36971bcd81b9d4c
jennyjoo <100cllr@gmail.com>
2024-07-09 11:25:12 +0900

    style: footer 구현

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,22 +1,33 @@
 import { ReactNode } from 'react';
 import Nav from './Nav';
+import ToolBarLink from './ui/ToolBarLink';
+import { EmployeeMenuList } from '../pages/employee/datas';
+import Footer from './Footer';
 
 type Prop = {
   children: ReactNode;
   navTitle?: string;
   option?: boolean;
+  toolBar?: boolean;
 };
 
-const Frame = ({ children, navTitle = '알바ON', option = true }: Prop) => {
+const Frame = ({
+  children,
+  navTitle = '알바ON',
+  option = true,
+  toolBar = false,
+}: Prop) => {
   return (
     <div className='h-[100vh]'>
       {option && <Nav title={navTitle} />}
+      {toolBar && <ToolBarLink options={EmployeeMenuList} />}
       <div
-        className='flex flex-col items-center overflow-y-scroll'
-        style={{ height: 'calc(100vh - 60px)' }}
+        className='flex flex-col items-center overflow-y-scroll '
+        style={{ height: 'calc(100vh - 110px)' }}
       >
-        <div className='w-[90%] h-full flex flex-col items-center '>
-          {children}
+        <div className='flex-grow flex flex-col items-center w-full  '>
+          <div className='flex-grow min-h-[90%] w-[90%] '>{children}</div>
+          <Footer />
         </div>
       </div>
     </div>
